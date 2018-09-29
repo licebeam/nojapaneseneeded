8d66a8b7c0e8af5208472b87293f1ced6452d089
Dustin Walker <dustinwalkerart@gmail.com>
2018-09-29 12:27:07 +0900

    updated jobs section

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -14,11 +14,11 @@ class MainContainer extends Component {
   state = {
     jobPosts: [
       {
-        job: { title: 'test', description: 'blah blah blah' },
+        job: { title: 'Google', description: 'Engineer', location: 'Tokyo', date: 'Sep 29', logo: 'https://zdnet1.cbsistatic.com/hub/i/2015/09/01/cb834e24-18e7-4f0a-a9bf-4c2917187d3f/83bb139aac01023dbf3e55a3d1789ad8/google-new-logo.png', fresh: true },
         id: 1
       },
       {
-        job: { title: 'test', description: 'blah blah blah' },
+        job: { title: 'Supreme', description: 'Marketing Director', location: 'Tokyo', date: 'Sep 29', logo: 'https://static.highsnobiety.com/wp-content/uploads/2018/01/16110157/supreme-box-logo-00.jpg', fresh: false },
         id: 2
       }
     ]
--- src/components/JobPost.jsx
+++ src/components/JobPost.jsx
@@ -7,15 +7,34 @@ const Job = styled.div`
   height: 100px;
   padding: 10px;
   border-bottom: 2px solid grey;
+  display: flex;
+  flex-direction: row;
+  .logo{
+    border-radius: 50%;
+    border: 2px solid black;
+    height: 40px;
+    width: 40px;
+    overflow: hidden;
+    .image{
+      object-fit: cover;
+      height: 40px;
+      width: 40px;
+    }
+  }
 `
 class JobPost extends Component {
   render() {
     const { job } = this.props
-    console.log(job)
     return (
-      <Job>
-        <h4>{job.job.title}</h4>
-        <div>{job.job.description}</div>
+      <Job onClick={() => alert('not available yet')}>
+        <div className="logo">
+          <img className="image" src={job.job.logo} alt={job.job.title} />
+        </div>
+        <div className="title">{job.job.title}</div>
+        <div className="description">{job.job.description}</div>
+        <div className="description">{job.job.location}</div>
+        <div className="date">{job.job.date}</div>
+        <div className="fresh">{job.job.fresh ? 'New' : null}</div>
       </Job>
     );
   }
