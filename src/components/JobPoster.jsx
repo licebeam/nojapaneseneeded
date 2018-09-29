0593b1eefc68ca7c7fc2ecac2a98ddb4127ed034
Dustin Walker <dustinwalkerart@gmail.com>
2018-09-29 18:18:59 +0900

    removed sections in JobPoster

--- src/components/JobPoster.jsx
+++ src/components/JobPoster.jsx
@@ -10,51 +10,7 @@ const JobPosterContainer = styled.div`
     display: flex; 
     flex-direction: column;
     margin-bottom: 40px;
-    .logo-title{
-      margin-top: 20px;
-      height: 100px;
-      display: flex;
-      flex-direction: row;
-       align-items: center;
-      .title{
-      font-size: 2rem;
-      padding: 10px;
-    }
-      .logo{
-      border-radius: 50%;
-      height: 60px;
-      width: 60px;
-      overflow: hidden;
-      .image{
-        object-fit: cover;
-        height: 60px;
-        width: 60px;
-      }
-    }
-    }   
-  .description{
-    flex: 1;
-      margin-top:20px;
-      font-size: 1.5rem;
-    }
-  .location{
-    flex: 1;
-    margin-top:20px;
-    font-size: 1.5rem;
-    color: grey;
-    margin-bottom: 40px;
-  }
-  .date{
-    font-size: 1.5rem;
-    color: grey;
-    padding: 20px;
-  }
-  .category{
-    font-size: .8rem;
-  }
-  .details{
-    min-height: 300px;
-  }
+    
   }
 `
 class JobPoster extends Component {
