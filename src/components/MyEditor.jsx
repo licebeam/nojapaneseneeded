3dc206ca537a0e416743b02fa2feddbcb43545b8
Dustin Walker <dustinwalkerart@gmail.com>
2018-10-01 21:14:41 +0900

    added posed

--- src/components/MyEditor.jsx
+++ src/components/MyEditor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Editor, EditorState } from 'draft-js';
-import '../components/Draft.css';
+import 'draft-js/dist/Draft.css';
 class MyEditor extends React.Component {
   constructor(props) {
     super(props);
